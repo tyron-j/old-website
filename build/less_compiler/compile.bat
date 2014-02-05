c9fad323c4ea1d3e583dffda98741ac1641fc001
tyron-j <tyronjung@hotmail.com>
2014-02-04 19:23:40 -0500

    About Page
    Finished the layout for the about page (about.html).

--- build/less_compiler/compile.bat
+++ build/less_compiler/compile.bat
@@ -2,6 +2,7 @@
 
 cd C:\Users\Tyron\Documents\GitHub\website\styles
 
+call lessc -x less/about.less > compiled/about.css
 call lessc -x less/base.less > compiled/base.css
 call lessc -x less/main.less > compiled/main.css
 call lessc -x less/portfolio.less > compiled/portfolio.css
