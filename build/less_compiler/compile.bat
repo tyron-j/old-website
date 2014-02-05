5f5e9accd486cbe7116fd9df4d51489d9a616a29
tyron-j <tyronjung@hotmail.com>
2014-02-05 05:55:43 -0500

    Resume Page and Popover

--- build/less_compiler/compile.bat
+++ build/less_compiler/compile.bat
@@ -6,5 +6,6 @@ call lessc -x less/about.less > compiled/about.css
 call lessc -x less/base.less > compiled/base.css
 call lessc -x less/main.less > compiled/main.css
 call lessc -x less/portfolio.less > compiled/portfolio.css
+call lessc -x less/resume.less > compiled/resume.css
 
 cd C:\Users\Tyron\Documents\GitHub\website
