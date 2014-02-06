30e4c357b148de59c7a5997b2f73a7889fe3a4ad
tyron-j <tyronjung@hotmail.com>
2014-02-06 14:49:31 -0500

    Gallery

--- build/less_compiler/compile.bat
+++ build/less_compiler/compile.bat
@@ -5,6 +5,7 @@ cd C:\Users\Tyron\Documents\GitHub\website\styles
 call lessc -x less/about.less > compiled/about.css
 call lessc -x less/base.less > compiled/base.css
 call lessc -x less/externals.less > compiled/externals.css
+call lessc -x less/gallery.less > compiled/gallery.css
 call lessc -x less/main.less > compiled/main.css
 call lessc -x less/portfolio.less > compiled/portfolio.css
 call lessc -x less/resume.less > compiled/resume.css
