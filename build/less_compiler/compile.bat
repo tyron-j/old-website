b95becb86712b6876926457c04d3be8453dedbc9
tyron-j <tyronjung@hotmail.com>
2014-02-06 03:10:39 -0500

    Slideshow

--- build/less_compiler/compile.bat
+++ build/less_compiler/compile.bat
@@ -8,5 +8,6 @@ call lessc -x less/externals.less > compiled/externals.css
 call lessc -x less/main.less > compiled/main.css
 call lessc -x less/portfolio.less > compiled/portfolio.css
 call lessc -x less/resume.less > compiled/resume.css
+call lessc -x less/slideshow.less > compiled/slideshow.css
 
 cd C:\Users\Tyron\Documents\GitHub\website
