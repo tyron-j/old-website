b714e7e54c8772976e8d65f4745bd28ea10343ce
Tyron Jung <tyronjung@hotmail.com>
2014-06-07 22:59:39 -0400

    Implemented Root.UI.IFrame

--- root/modules/UI/IFrame.js
+++ root/modules/UI/IFrame.js
@@ -2,8 +2,8 @@
 
 
 
-Root.import(['Root.UI.Behavior', 'Root.UI.IFrame.ScrollBar', 'Root.UI.IFrame.Scroller', 'Root.UI.IFrame.ScrollContent'],
-	function (Behavior, ScrollBar, Scroller, ScrollContent) {
+Root.import(['Root.UI.Behavior'],
+	function (Behavior) {
 
 		Root.export('Root.UI.IFrame', Root.classify({
 
@@ -12,41 +12,70 @@ Root.import(['Root.UI.Behavior', 'Root.UI.IFrame.ScrollBar', 'Root.UI.IFrame.Scr
 			initialize: function (node, options) { // needs testing
 				this.callSuper('initialize', node);
 
+				// create essential elements
 				var scrollBar = node.appendChild(document.createElement('div')),
 					scroller = scrollBar.appendChild(document.createElement('div')),
 					scrollWindow = node.appendChild(document.createElement('div')),
-					scrollContent = scrollWindow.appendChild(document.createElement('div'));
+					scrollContent = scrollWindow.appendChild(document.createElement('div')),
 
-					// content = node.innerHTML; // consider changing this
-					// node.innerHTML = '';
+					that = this;
 
-				if (!node.offsetWidth) {
-					this.setStyle({
-						width: Root.UI.IFrame.width,
-						height: Root.UI.IFrame.height
-					});
-				}
-
+				// initialize styles
 				scrollBar.classList.add(options && options.scrollBarClass || 'ScrollBar');
 				scroller.classList.add(options && options.scrollerClass || 'Scroller');
 				scrollWindow.classList.add(options && options.scrollWindowClass || 'ScrollWindow');
 				scrollContent.classList.add(options && options.scrollContentClass || 'ScrollContent');
 
-				this.scrollerMax = scrollBar.offsetHeight - scroller.offsetHeight;
-				this.scrollContentMax = scrollContent.offsetHeight - scrollWindow.offsetHeight; // this might be too soon
+				// for testing
+				scrollBar.classList.add('FaintWhite');
+				scroller.classList.add('Teal');
 
-				/* this.scrollBar = */new ScrollBar(scrollBar, this);
-				/* this.scroller = */new Scroller(scroller, this);
-				/* this.scrollContent = */new ScrollContent(scrollContent, this);
+				// initialize settings
+				Root.consolidate(this, {
+					scrollerMax: scrollBar.offsetHeight - scroller.offsetHeight,
+					scrollContentMax: scrollContent.offsetHeight - scrollWindow.offsetHeight,
+					body: new Behavior(document.body)
+				});
 
-				this.body = new Behavior(document.body);
+				// event handlers
 
-				// this.scrollContent.innerHTML = content;
+				(new Behavior(scrollBar)).handle({
+					mousedown: function (evt) {
+						that.setAbsoluteTop(scrollBar);
+						that.moveScroller(evt, scrollBar, scroller, scrollContent);
+					}
+				});
+
+				(new Behavior(scroller)).handle({
+					mousedown: function (evt) {
+						evt.preventDefault(); // check what this does
+
+						that.currentTop = parseInt(scroller.style.top) || 0;
+						that.initialY = evt.clientY;
+
+						that.body.handle('mousemove', function (evt) {
+							that.startDrag(evt, scroller, scrollContent);
+						});
+						that.body.handle('mouseup', function () {
+							that.stopDrag();
+						});
+					}
+				});
+
+				(new Behavior(scrollWindow)).handle({
+					mousewheel: function (evt) {
+						evt.preventDefault(); // prevent default scrolling
+
+						that.currentTop = parseInt(scroller.style.top) || 0;
+
+						evt.wheelDelta > 0 ? that.scrollUp(scroller, scrollContent) : that.scrollDown(scroller, scrollContent);
+					}
+				});
 			},
 
 			methods: {
 
-				setAbsoluteTop: function (scrollBar) { // scrollBar is the element
+				setAbsoluteTop: function (scrollBar) {
 					var absoluteTop = 0;
 
 					for (var e = scrollBar; e; e = e.offsetParent) {
@@ -60,10 +89,10 @@ Root.import(['Root.UI.Behavior', 'Root.UI.IFrame.ScrollBar', 'Root.UI.IFrame.Scr
 					if (scrollBar === evt.target) {
 						this.currentTop = parseInt(scroller.style.top) || 0;
 
-						var cursorPosition = evt.pageY - this.absoluteTop,
-							delta = cursorPosition - (this.currentTop - scroller.offsetHeight / 2);
+						var cursorPos = evt.pageY - this.absoluteTop,
+							delta = cursorPos - (this.currentTop + scroller.offsetHeight / 2);
 
-						scroller.style.top = Math.max(Math.min(this.currentTop + delta, this.scrollerMax), 0); + 'px';
+						scroller.style.top = Math.max(Math.min(this.currentTop + delta, this.scrollerMax), 0) + 'px';
 
 						this.reposition(scroller, scrollContent);
 					}
@@ -84,8 +113,8 @@ Root.import(['Root.UI.Behavior', 'Root.UI.IFrame.ScrollBar', 'Root.UI.IFrame.Scr
 				},
 
 				stopDrag: function () { // needs testing
-					this.body.ignore('mousemove', this.boundStartDrag);
-					this.body.ignore('mousemove', this.stopDrag);
+					this.body.ignore('mousemove');
+					this.body.ignore('mouseup');
 				},
 
 				scrollUp: function (scroller, scrollContent) {
@@ -95,16 +124,16 @@ Root.import(['Root.UI.Behavior', 'Root.UI.IFrame.ScrollBar', 'Root.UI.IFrame.Scr
 				},
 
 				scrollDown: function (scroller, scrollContent) {
-					scroller.style.top = Math.min(this.currentTop + 50, 0) + 'px';
+					scroller.style.top = Math.min(this.currentTop + 50, this.scrollerMax) + 'px';
 
 					this.reposition(scroller, scrollContent);
 				}
 
-			}
+			},
 
 			statics: {
 				width: '500px',
-				heightL '500px'
+				height: '500px'
 			}
 
 		}));
