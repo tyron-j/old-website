dff6610411d29bd8dd355a570dbed2ff95d51d1a
Tyron Jung <tyronjung@hotmail.com>
2014-06-16 00:40:42 -0400

    Removed unnecessary wrapper for SlideShow.js

--- root/modules/UI/SlideShow.js
+++ root/modules/UI/SlideShow.js
@@ -2,58 +2,54 @@
 
 
 
-(function () {
+Root.import(['Ajax', 'Task', 'UI/Behavior'],
+	function (Ajax, Task, Behavior) {
 
-	Root.import(['Ajax', 'Task', 'UI/Behavior'],
-		function (Ajax, Task, Behavior) {
+		var SlideShow = Root.classify({
 
-			var SlideShow = Root.classify({
+			extend: Behavior,
 
-				extend: Behavior,
+			initialize: function (node, options) {
+				var that = this;
 
-				initialize: function (node, options) {
-					var that = this;
+				this.getSlides(options.src).onSuccess(function (slides) { // to-do: configure options properly
+					//
+				});
+			},
 
-					this.getSlides(options.src).onSuccess(function (slides) { // to-do: configure options properly
-						//
-					});
-				},
+			methods: {
 
-				methods: {
+				getSlides: function (url) {
+					var task = new Task();
 
-					getSlides: function (url) {
-						var task = new Task();
+					Ajax.get(url).onSuccess(function (res) { // to-do: add a fail condition
+						var slides = res.match(/href\=\".+\"/g); // get the file names; to-do: make sure they're image files
 
-						Ajax.get(url).onSuccess(function (res) { // to-do: add a fail condition
-							var slides = res.match(/href\=\".+\"/g); // get the file names; to-do: make sure they're image files
+						slides.shift(); // remove parent directory
 
-							slides.shift(); // remove parent directory
+						slides = slides.map(function (slide) {
+							var div = document.createElement('div');
 
-							slides = slides.map(function (slide) {
-								var div = document.createElement('div');
+							div.style.backgroundImage = 'url(' + url + '/' + slide.replace(/^href\=/, '').replace(/\"/g, '') + ')';
 
-								div.style.backgroundImage = 'url(' + url + '/' + slide.replace(/^href\=/, '').replace(/\"/g, '') + ')';
-
-								return div;
-							});
-
-							task.resolve(slides);
+							return div;
 						});
 
-						return task;
-					}
+						task.resolve(slides);
+					});
 
-				},
-
-				statics: {
-					//
+					return task;
 				}
 
-			});
+			},
 
-			Root.export('UI/SlideShow', SlideShow);
+			statics: {
+				//
+			}
 
-		}
-	);
+		});
 
-})();
+		Root.export('UI/SlideShow', SlideShow);
+
+	}
+);
