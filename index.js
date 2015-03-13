8603f3436e8e9d9aac92caa234f191404c5f1786
Tyron Jung <tyronjung@hotmail.com>
2015-03-13 16:01:48 -0700

    First Commit
    Setting up Node server

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+	res.send("Tyron Jung's Node-based website!");
+});
+
+app.listen(app.get('port'), function () {
+	console.log("App running at localhost:" + app.get('port'));
+});
