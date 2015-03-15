2d72c639c98ba462816c08de5980a4bcadc5dfef
Tyron Jung <tyronjung@hotmail.com>
2015-03-15 16:08:24 -0700

    Refined Database Connection

--- index.js
+++ index.js
@@ -3,19 +3,31 @@ var express = require('express'),
 
 	app = express();
 
-/*if (process.env.LOCAL_HOST) {
-	mongoose.connect(process.env.MONGOLAB_URI.replace("<dbuser>:<dbpassword>", process.env.MASTER_LOGIN));
-} else {
-	mongoose.connect(process.env.MONGOLAB_URI.replace("<dbuser>:<dbpassword>", process.env.GUEST_LOGIN));
-}*/
+// database connection callbacks
+mongoose.connection.on('connected', function () {
+	console.log("Successfully connected to database");
+}).on('error', function (err) {
+	console.error("Failed to connect to database: " + err.errmsg);
+});
 
+// connect to database
+if (JSON.parse(process.env.LOCAL_HOST)) {
+	console.log("Connecting to database as master");
+	mongoose.connect(process.env.MONGOLAB_URI.replace('<dbuser>:<dbpassword>', process.env.MASTER_LOGIN));
+} else {
+	console.log("Connecting to database as guest");
+	mongoose.connect(process.env.MONGOLAB_URI.replace('<dbuser>:<dbpassword>', process.env.GUEST_LOGIN));
+}
+
+// configure app
 app.set('port', (process.env.PORT || 9000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.send(Object.keys(process.env));
+	res.send("Tyron Jung's Node.js Website!");
 });
 
+// start app
 app.listen(app.get('port'), function () {
-	console.log("App running at localhost:" + app.get('port'));
+	console.log("Running at localhost:" + app.get('port'));
 });
