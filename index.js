0bf58232719f712fb854bd70eb8f664399b231c4
Tyron Jung <tyronjung@hotmail.com>
2015-03-15 15:25:44 -0700

    Checking Heroku Config Vars

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express'),
 	mongoose = require('mongoose'),
+
 	app = express();
 
-mongoose.connect(process.env.MONGOLAB_URI);
-app.set('port', (process.env.PORT || 5000));
+/*if (process.env.LOCAL_HOST) {
+	mongoose.connect(process.env.MONGOLAB_URI.replace("<dbuser>:<dbpassword>", process.env.MASTER_LOGIN));
+} else {
+	mongoose.connect(process.env.MONGOLAB_URI.replace("<dbuser>:<dbpassword>", process.env.GUEST_LOGIN));
+}*/
+
+app.set('port', (process.env.PORT || 9000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.send("Tyron Jung's Node.js Website!");
+	res.send(Object.keys(process.env));
 });
 
 app.listen(app.get('port'), function () {
