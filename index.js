89eb3a36ffeb548bf88abcb1bc786f81eb6a46a8
Tyron Jung <tyronjung@hotmail.com>
2015-03-14 23:07:45 -0700

    Database Verification

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.send(Object.keys(db));
+	res.send("Hello from " + db.name + "!");
 });
 
 app.listen(app.get('port'), function () {
