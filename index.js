b2f5553db48ceaed68ab4af083feeaa65c5deec8
Tyron Jung <tyronjung@hotmail.com>
2015-03-14 22:55:07 -0700

    Checking process.env Keys

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var app = express();
 
+// db logic
+
+// var mongoose = require('mongoose');
+
+// end of db logic
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.send("Tyron Jung's Node.js Website!");
+	res.send(Object.keys(process.env));
 });
 
 app.listen(app.get('port'), function () {
