8d55bfd48572fe080b76afe7819fa1f0c611bf1b
Tyron Jung <tyronjung@hotmail.com>
2015-03-14 23:10:49 -0700

    Done Testing

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
-var express = require('express');
-var app = express();
-
-// db logic
-
-var mongoose = require('mongoose'),
-	db;
+var express = require('express'),
+	mongoose = require('mongoose'),
+	app = express();
 
 mongoose.connect(process.env.MONGOLAB_URI);
-db = mongoose.connection;
-
-// end of db logic
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.send("Hello from " + db.name + "!");
+	res.send("Tyron Jung's Node.js Website!");
 });
 
 app.listen(app.get('port'), function () {
