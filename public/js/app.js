5ee58b6b0332078da7ebd68a3806a5af4d96ebda
Tyron Jung <tyronjung@hotmail.com>
2015-04-12 16:11:53 -0700

    Cloaking
    Also preventing minification errors from app config.

--- public/js/app.js
+++ public/js/app.js
@@ -12,43 +12,48 @@ define([
 		'ngAnimate',
 		'ngRoute',
 		'allControllers'
-	]).config(function ($routeProvider, $locationProvider) { // minification errors?
-		// public routes
-		$routeProvider.when('/login', {
-			templateUrl: '/partials/login',
-			controller: 'LoginCtrl'
-		}).when('/gallery', { // currently localhost only
-			templateUrl: '/partials/gallery',
-			controller: 'GalleryCtrl'
-		}).when('/blog', { // currently localhost only
-			templateUrl: '/partials/blog',
-			controller: 'BlogCtrl'
-		}).when('/hello', {
-			templateUrl: '/partials/hello'
-		}).when('/unauthorized', {
-			templateUrl: '/partials/unauthorized'
-		}).when('/success', {
-			templateUrl: '/partials/success'
-		});
+	]).config([
+		'$routeProvider',
+		'$locationProvider',
+		
+		function ($routeProvider, $locationProvider) {
+			// public routes
+			$routeProvider.when('/login', {
+				templateUrl: '/partials/login',
+				controller: 'LoginCtrl'
+			}).when('/gallery', { // currently localhost only
+				templateUrl: '/partials/gallery',
+				controller: 'GalleryCtrl'
+			}).when('/blog', { // currently localhost only
+				templateUrl: '/partials/blog',
+				controller: 'BlogCtrl'
+			}).when('/hello', {
+				templateUrl: '/partials/hello'
+			}).when('/unauthorized', {
+				templateUrl: '/partials/unauthorized'
+			}).when('/success', {
+				templateUrl: '/partials/success'
+			});
 
-		// master routes
-		$routeProvider.when('/master/upload', {
-			templateUrl: '/partials/master/upload'
-		}).when('/master/gallery', {
-			templateUrl: '/partials/master/gallery',
-			controller: 'MasterGalleryCtrl'
-		}).when('/master/blog', {
-			templateUrl: '/partials/master/blog',
-			controller: 'MasterBlogCtrl'
-		});
+			// master routes
+			$routeProvider.when('/master/upload', {
+				templateUrl: '/partials/master/upload'
+			}).when('/master/gallery', {
+				templateUrl: '/partials/master/gallery',
+				controller: 'MasterGalleryCtrl'
+			}).when('/master/blog', {
+				templateUrl: '/partials/master/blog',
+				controller: 'MasterBlogCtrl'
+			});
 
-		// otherwise
-		$routeProvider.otherwise({
-			redirectTo: '/login'
-		});
+			// otherwise
+			$routeProvider.otherwise({
+				redirectTo: '/login'
+			});
 
-		$locationProvider.html5Mode(true);
-	});
+			$locationProvider.html5Mode(true);
+		}
+	]);
 
 	ng.bootstrap(document, ['tyronApp']);
 
