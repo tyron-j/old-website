8ce7e7dc3240f4876d5fab492a042325876c40e6
Tyron Jung <tyronjung@hotmail.com>
2015-04-14 21:13:08 -0700

    Front End Blog Fetching Logic

--- public/js/controllers/blogctrl.js
+++ public/js/controllers/blogctrl.js
@@ -12,11 +12,25 @@ define(function () {
 				$scope.sideBar.inUse   = true;
 				$scope.sideBar.title   = 'Blogs';
 				$scope.sideBar.items   = blogs;
+
+				// this function varies based on the side bar's content
+				$scope.sideBar.selectItem = function (blog) { // to-do: use a service?
+					if (!blog.content) { // if not already fetched
+						$http.get('/api/blog/' + blog.title).success(function (blogContent) {
+							blog.content = blogContent;
+						});
+					}
+
+					$scope.sideBar.selectedItem = blog;
+				};
+
+				$scope.sideBar.selectItem(blogs[0]);
 			});
 
 			// destruction
 			$scope.$on('$locationChangeStart', function (evt, next, current) {
 				$scope.sideBar.inUse   = false;
+				$scope.sideBar.selectedItem = null;
 				// $scope.sideBar.title   = '';
 				// $scope.sideBar.items   = [];
 			});
