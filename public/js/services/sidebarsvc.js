918a5d72bd7f6e483001588417ab39e36af9d27d
Tyron Jung <tyronjung@hotmail.com>
2015-04-22 17:47:05 -0700

    New Blog Button

--- public/js/services/sidebarsvc.js
+++ public/js/services/sidebarsvc.js
@@ -5,14 +5,20 @@ define(function () {
 
 	return [
 		'$http',
+		'$location',
 
-		function ($http) { // use as factory
+		function ($http, $location) { // use as factory
 			return {
 				model: { // using a singleton since there will only be one instance of a side bar
 					inUse: false,
 					title: '',
 					items: [],
-					selectItem: function () {}
+					selectItem: function () {},
+					newItem: {
+						allowed: false,
+						title: '',
+						create: function () {}
+					}
 				},
 
 				blogMode: function ($scope) {
@@ -25,17 +31,30 @@ define(function () {
 						sideBar.items      = blogs;
 						sideBar.selectItem = that.selectBlog;
 
+						if ($location.path() === '/master/blog') {
+							sideBar.newItem.allowed = true;
+							sideBar.newItem.title   = 'New Blog';
+							sideBar.newItem.create  = that.createBlog;
+						}
+
 						sideBar.selectItem(blogs[0]);
 					});
 
 					// to-do: make this an individual method
 					$scope.$on('$locationChangeStart', function (evt, next, current) {
-						sideBar.inUse        = false;
-						// sideBar.title        = '';
-						// sideBar.items        = [];
-						// sideBar.selectItem   = function () {};
-						sideBar.selectedItem = null;
+						sideBar.inUse             = false;
+						// sideBar.title          = '';
+						// sideBar.items          = [];
+						// sideBar.selectItem     = function () {};
+						sideBar.selectedItem      = null;
+						sideBar.newItem.allowed   = false;
+						// sideBar.newItem.title  = '';
+						// sideBar.newItem.create = function () {};
 					});
+				},
+
+				createBlog: function () {
+					//
 				},
 
 				selectBlog: function (blog) { // this function varies based on the side bar's content
