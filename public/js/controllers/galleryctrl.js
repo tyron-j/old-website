fa13aa6f1d71973801c9bdc5ce1c9d6edd89e374
Tyron Jung <tyronjung@hotmail.com>
2015-05-29 21:26:17 -0700

    Edit

--- public/js/controllers/galleryctrl.js
+++ public/js/controllers/galleryctrl.js
@@ -16,7 +16,7 @@ define(function () {
 
 			$scope.modalImage = modalImageSvc.model;
 
-			// disregard scroll bar width
+			// disregarding scroll bar width
 			var gallery        = document.getElementsByClassName('gallery')[0];
 			var scrollBarWidth = gallery.offsetWidth - gallery.clientWidth;
 
