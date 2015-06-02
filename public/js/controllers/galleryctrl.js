a13ed3e79a24bd746a2ea88fbcd64b4cff4dc2af
Tyron Jung <tyronjung@hotmail.com>
2015-06-01 21:20:56 -0700

    Scroll Bar Style

--- public/js/controllers/galleryctrl.js
+++ public/js/controllers/galleryctrl.js
@@ -16,6 +16,7 @@ define(function () {
 
 			$scope.modalImage = modalImageSvc.model;
 
+			// to-do: use custom ::-webkit-scrollbar styles
 			// disregarding scroll bar width
 			var gallery        = document.getElementsByClassName('gallery')[0];
 			var scrollBarWidth = gallery.offsetWidth - gallery.clientWidth;
