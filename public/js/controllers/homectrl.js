5cb3c6cc311818b1f226030485db83d6d018d1ad
Tyron Jung <tyronjung@hotmail.com>
2015-07-21 23:32:53 -0700

    Home News Transition Partially Working

--- public/js/controllers/homectrl.js
+++ public/js/controllers/homectrl.js
@@ -5,8 +5,9 @@ define(function () {
 
 	return [
 		'$scope',
+		'$timeout',
 		
-		function ($scope) {
+		function ($scope, $timeout) {
 			$scope.homeLinks = [{
 				title: 'About',
 				href: '/about'
@@ -25,7 +26,31 @@ define(function () {
 			}, {
 				title: 'Special',
 				href: '/special'
-			}]
+			}];
+
+			$scope.homeNews = [{ // to-do: fetch these from db via http
+				shown: true,
+				src: 'http://i.imgur.com/jwD87Hu.jpg'
+			}, {
+				shown: false,
+				src: 'https://data.archive.moe/board/a/image/1418/58/1418586274250.jpg'
+			}];
+
+			var timeoutPromise;
+
+			var switchImage = function () {
+				$scope.homeNews[0].shown = !$scope.homeNews[0].shown;
+				$scope.homeNews[1].shown = !$scope.homeNews[1].shown;
+
+				timeoutPromise = $timeout(switchImage, 5000);
+			};
+
+			var ignoreLocationChangeStart = $scope.$on('$locationChangeStart', function (evt, next, current) {
+				$timeout.cancel(timeoutPromise);
+				ignoreLocationChangeStart();
+			});
+
+			timeoutPromise = $timeout(switchImage, 5000);
 		}
 	];
 });
