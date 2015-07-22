1cf79a3f7398dc496f73cdec5314546e9a6e8268
Tyron Jung <tyronjung@hotmail.com>
2015-07-21 17:05:06 -0700

    Home Link Styles

--- public/js/controllers/homectrl.js
+++ public/js/controllers/homectrl.js
@@ -7,7 +7,25 @@ define(function () {
 		'$scope',
 		
 		function ($scope) {
-			//
+			$scope.homeLinks = [{
+				title: 'About',
+				href: '/about'
+			}, {
+				title: 'Blog',
+				href: '/blog'
+			}, {
+				title: 'Gallery',
+				href: '/gallery'
+			}, {
+				title: 'Novel',
+				href: '/novel'
+			}, {
+				title: 'Resume',
+				href: '/resume'
+			}, {
+				title: 'Special',
+				href: '/special'
+			}]
 		}
 	];
 });
