0fee30d78c91085bb558a130cfc1643e1199a49a
Tyron Jung <tyronjung@hotmail.com>
2015-07-24 00:40:55 -0700

    About Look and Feel

--- public/js/controllers/aboutctrl.js
+++ public/js/controllers/aboutctrl.js
@@ -7,7 +7,9 @@ define(function () {
 		'$scope',
 		
 		function ($scope) {
-			//
+			$scope.displayPicture = {
+				src: 'https://38.media.tumblr.com/11120cce3ebd398acc5919b766b88340/tumblr_nkf2g1NHzx1timq39o1_500.gif'
+			};
 		}
 	];
 });
