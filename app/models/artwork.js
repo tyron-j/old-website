73386aff6788cbe4d830556de1c682a6b08c5bc0
Tyron Jung <tyronjung@hotmail.com>
2015-07-30 17:59:03 -0700

    Direction

--- app/models/artwork.js
+++ app/models/artwork.js
@@ -1,5 +1,7 @@
 // artwork
 
+// to-do: change to image and make the schema multi-purpose
+
 var db = require('../utils/db');
 
 module.exports = db.model('Artwork', new db.Schema({ // to-do: add creationDate
