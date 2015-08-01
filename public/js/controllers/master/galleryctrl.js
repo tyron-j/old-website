6e0f90aa096d33aee19307fe8aa399ecc35d13ff
Tyron Jung <tyronjung@hotmail.com>
2015-08-01 00:27:38 -0700

    Master Gallery Combo Box

--- public/js/controllers/master/galleryctrl.js
+++ public/js/controllers/master/galleryctrl.js
@@ -8,15 +8,17 @@ define(function () {
 		'$scope',
 
 		'buttonBarSvc',
+		'comboBoxSvc',
 		'modalImageSvc',
 		
-		function ($http, $scope, buttonBarSvc, modalImageSvc) {
+		function ($http, $scope, buttonBarSvc, comboBoxSvc, modalImageSvc) {
 			$http.get('/api/image').success(function (res) {
 				$scope.images = res;
 			});
 
 			$scope.imageThumbnailEditor = buttonBarSvc.getImageThumbnailEditor($scope);
 			$scope.imageBrowserEditor   = buttonBarSvc.getImageBrowserEditor($scope);
+			$scope.imageBrowserSelector = comboBoxSvc.imageBrowserSelector;
 			$scope.modalImage           = modalImageSvc.model;
 
 			// to-do: consider making image browser a widget and moving these methods to a service
