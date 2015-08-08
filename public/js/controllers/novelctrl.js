4a9edc1556183751442da3142d9b657b552c256f
Tyron Jung <tyronjung@hotmail.com>
2015-08-08 04:04:53 -0700

    Novel Teaser

--- public/js/controllers/novelctrl.js
+++ public/js/controllers/novelctrl.js
@@ -4,10 +4,23 @@ define(function () {
 	'use strict';
 
 	return [
+		'$http',
 		'$scope',
 		
-		function ($scope) {
-			//
+		function ($http, $scope) {
+			$http.get('/api/image/novel').success(function (res) {
+				$scope.images = res;
+			});
+
+			$scope.firstImageLoaded = false;
+
+			$scope.handleImageLoad = function (evt, image) {
+				if (!$scope.firstImageLoaded) {
+					$scope.firstImageLoaded = true;
+				}
+
+				image.loaded = true;
+			};
 		}
 	];
 });
