9b31ed42465456a0869a34feaf929a1e066bd3db
Tyron Jung <tyronjung@hotmail.com>
2015-08-14 23:34:54 -0700

    Login Agent Rough Logic

--- public/js/controllers/loginctrl.js
+++ public/js/controllers/loginctrl.js
@@ -9,10 +9,45 @@ define(function () {
 		'$timeout',
 		
 		function ($cookies, $scope, $timeout) {
-			// to-do: make login agent a widget
 			$timeout(function () {
 				$scope.loginAgentShown = true;
 			}, 1000);
+
+			var loginAgent = { // singleton; to-do: consider turning this into a global widget
+				questionMode: false,
+				questionCallback: function () {
+					console.error("No question was passed into the login agent"); // default logic
+				},
+				textDelay: 3000, // constant for how long each text is displayed
+				text: '',
+
+				askQuestion: function (question, callback) {
+					this.questionMode     = true;
+					this.questionCallback = callback || this.questionCallback;
+					this.text             = question || '';
+				},
+
+				receiveAnswer: function (answer) {
+					this.questionMode = false;
+
+					this.questionCallback(answer);
+				},
+
+				// textQueue must be array
+				spewText: function (textQueue, callback) {
+					if (textQueue.length) {
+						var that = this;
+
+						this.text = textQueue.shift();
+
+						$timeout(function () {
+							that.spewText(textQueue, callback);
+						}, this.textDelay);
+					} else {
+						callback();
+					}
+				}
+			};
 		}
 	];
 });
