854389987e642c82a8850e1e9c650562bea61f89
Tyron Jung <tyronjung@hotmail.com>
2015-08-14 18:05:22 -0700

    Direction

--- public/js/controllers/loginctrl.js
+++ public/js/controllers/loginctrl.js
@@ -9,6 +9,7 @@ define(function () {
 		'$timeout',
 		
 		function ($cookies, $scope, $timeout) {
+			// to-do: make login agent a widget
 			$timeout(function () {
 				$scope.loginAgentShown = true;
 			}, 1000);
